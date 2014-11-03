aeff9ef70c2d21987d6eef0b5a7b670fe6f14be8
mi222eh <mi222eh@student.lnu.se>
2014-11-03 19:34:22 +0000

    Added code in guess and variables outside

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,15 +2,33 @@
 
 window.onload = function(){
 	
-	var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
+	var secret = Math.floor(Math.random() * 100 ) +1; // Ett slumpat tal mellan 1-100.
+	var NumberOfGuesses = 0; //Variabel för antalet gissningar
 	
 	// I denna funktion ska du skriva koden för att hantera "spelet"
 	var guess = function(number){
 		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
 		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
-			
-		// Plats för förändring.
+		
+		
+		
+		NumberOfGuesses += 1;
+		if (number < 0 && number > 100){
+			return [false, "Talet är utanför intervallet 0 - 100"];
+		}
+		else if (number == secret) {
+			return [true, "Grattis du vann! Det hemliga talet var " + secret + " och du behövde " + NumberOfGuesses + " gissningar för att hitta det."];
+		}
+		
+		else if (number < secret) {
+			return [false, "Det hemliga talet är högre!"];
+		}
+		
+		else if (number > secret) {
+			return [false, "Det hemliga talet är lägre!"];
+		}
 
+		
 
 		// Returnera exempelvis: 
 		// [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
