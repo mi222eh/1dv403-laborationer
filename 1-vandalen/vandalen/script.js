9406db38e538ef28c960fe01a7f7c915ec349f30
mi222eh <mi222eh@student.lnu.se>
2014-11-06 15:48:09 +0000

    Added code to makePerson function

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,9 +1,58 @@
 "use strict";
 
 var makePerson = function(persArr){
-
-
-	// Din kod här...
-
+    //deklarera
+    var names, ages, object, average, stringNames;
+    
+    
+    object = {};
+    names = [];
+    ages = [];
+    
+    //Kör en function för alla element:
+    persArr.forEach(function(Element, Index){
+        names[Index] = Element.name;
+        ages[Index] = Element.age;
+    });
+    
+    //Sortera åldern
+    ages.sort(function(a, b){
+        return a-b;
+    });
+    
+    //Plussa ihop alla åldrar
+    average = ages.reduce(function(a, b){
+       return a+b; 
+    });
+    
+    //Räkna ut medelåldern
+    average = average/(persArr.length);
+    
+    //Avrunda medelåldern
+    average = Math.round(average);
+    
+    
+    
+    //Sortera namnen, skickar med en funktion som beskriver sorteringen
+    names.sort(function(a,b){
+        var nameA,nameB
+        nameA = a.toLowerCase();
+        nameB = b.toLowerCase();
+        
+        return nameA.localeCompare(nameB, 'sv');
+        
+    });
+    
+    //Gör om arrayen med namnen till en sträng separerade med ", "
+    stringNames = names.join(", ");
+    
+    //Mata in all data i objektet
+    object.minAge = ages[0];
+    object.maxAge = ages[ages.length - 1];
+    object.averageAge = average;
+    object.names = stringNames;
+    
+    //Returnera den
+    return object;
 }
 
