be3c20d91e84c227501f2684b31e1383a7785acc
mi222eh <mi222eh@student.lnu.se>
2014-11-18 01:31:58 +0000

    Added function to button

--- 2-labbymezzage/JS/init.js
+++ 2-labbymezzage/JS/init.js
@@ -1,5 +1,17 @@
 var init = {
+    messageNumber : 0,
     messages: [],
-    run: function(){
+    run: function(text){
+        var message = new Message(text, new Date());
+        init.messages.push(message);
+    },
+    readInput: function(){
+        var input;
+        input = document.getElementById("inputField").value;
+        init.run(input);
+    },
+    writeMessages: function(){
+        var area = queryselector("#messageArea");
+        
     }
 };
