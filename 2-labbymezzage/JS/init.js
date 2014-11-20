74ccd493a927780381d6a97c82b1f2150cb20ca8
mi222eh <mi222eh@student.lnu.se>
2014-11-20 13:36:47 +0000

    general fix

--- 2-labbymezzage/JS/init.js
+++ 2-labbymezzage/JS/init.js
@@ -1,50 +1,73 @@
 "use strict"
 
 var init = {
+    //räknare (för message listan)
     counter : 0,
+    
+    //räknare för antal meddelanden
     messageNumber : 0,
+    
+    //lista med meddelanden
     messages: [],
+    
+    //lista med månader
     month: ["Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli", "Augusti", "September", "Okteboer", "November", "December"],
-    init: function(){
+    
+    //funktion som rensar skrivfältet
+    clearInputField : function(){
         var input = document.getElementById("inputField");
-        var numberArea = document.getElementById("numberOfMessages");
-        numberArea.innerHTML = init.messageNumber;
         input.value = "";
     },
+    //funktion som håller reda på antal skrivna meddelanden och uppdaterar numret
+    init: function(){
+        var numberArea = document.getElementById("numberOfMessages");
+        numberArea.innerHTML = init.messageNumber;
+    },
+    
+    //funktion som läser av skrivfältet 
     readInput: function(){
         var input;
         input = document.getElementById("inputField").value;
         return input;
         
     },
+    
+    //då det skickas meddelande anropas denna funktion
     run: function(){
         
+        //läs in text
         var text = init.readInput();
+        
+        //kontrollera om den är tom
         if (text.trim() === ""){
             alert("Kan inte skicka in ett tomt meddelande");
             return;
         }
+        
         //skapa message objekt med hjälp av input
         var message = new Message(text, new Date());
-        init.messages.push(message);
+        this.messages.push(message);
         
         //skriva ut
-        init.writeMessages();
+        this.writeMessages();
         
         //räkna upp
-        init.counter += 1;
-        init.messageNumber +=1;
+        this.counter += 1;
+        this.messageNumber +=1;
         
         
         //scrolla längs ned
         var div = document.getElementById("messageField");
         div.scrollTop = div.scrollHeight;
         
-        init.init();
+        //uppdatera sidan (och rensa)
+        this.init();
+        this.clearInputField();
         
     },
     writeMessages: function(){
-        //Create elements
+        
+        //Skapa element
         var divImages = document.createElement('div');
         var div = document.createElement('div');
         var node = document.createElement('p');
@@ -54,10 +77,10 @@ var init = {
         var imgTime = document.createElement('img');
         var aTime = document.createElement('a');
         
-        //get the area
+        //Fånga området
         var area = document.getElementById("messageField");
         
-        //configure elements
+        //Konfigurera elementen
         divImages.setAttribute("class", "floatRight");
         div.setAttribute("id", init.counter);
         img.setAttribute("src", "pics/remove.png");
@@ -65,52 +88,90 @@ var init = {
         imgTime.setAttribute("alt", "Se Tiden");
         imgTime.setAttribute("src", "pics/time.png");
         aTime.setAttribute("href","#");
-        aTime.setAttribute("onClick", "init.time(" + init.counter + ")");
-        a.setAttribute("onClick", "init.removeMessage("+ init.counter + ")");
+        aTime.addEventListener("click", function(){
+            init.time(a.parentNode.parentNode.parentNode.getAttribute('id'));
+        });
         a.setAttribute("href","#");
-
-        //create text nodes
+        a.addEventListener("click", function(){
+            init.removeMessage(a.parentNode.parentNode.parentNode.getAttribute('id'));
+        });
+        
+        //Lägg in text
         var text = init.messages[init.counter].getHTMLText();
         var date = document.createTextNode(init.messages[init.counter].getDateText());
         
-        //put everything in place
-        span.appendChild(date);
-        aTime.appendChild(imgTime);
-        a.appendChild(img);
-        divImages.appendChild(a);
-        divImages.appendChild(aTime);
-        node.innerHTML = text;
-        node.insertBefore(divImages, node.childNodes[0]);
-        node.appendChild(span);
-        div.appendChild(node);
-        area.appendChild(div);
+        //Sätt allt på plats...
+        
+        span.appendChild(date);                             //...Date in i span
+        aTime.appendChild(imgTime);                         //...Tidsbilden in i a-taggen för tiden
+        a.appendChild(img);                                 //...TaBortBilden in i a-taggen för ta bort
+        divImages.appendChild(a);                           //...a-taggen in i div av bilder
+        divImages.appendChild(aTime);                       //...a-taggen in i div av bilder (samma som ovan)
+        node.innerHTML = text;                              //...lägg in meddelandet i p-taggen (kallas för node i detta fall)
+        node.insertBefore(divImages, node.childNodes[0]);   //...sätt div med bilder in i p-taggen FÖRE texten
+        node.appendChild(span);                             //...sätt datumen (som finns i span) sist i p-taggen
+        div.appendChild(node);                              //...sätt in p-taggen i en div
+        area.appendChild(div);                              //...sätt div in i området för texten (sist)
         
         
     },
+    
+    //funktion av borttagning av meddelande
     removeMessage: function (messageId){
+        
+        //Bekräfta ut av användaren
         if (confirm("Vill du verkligen radera meddelandet?")){
-            var tmpMessage = document.getElementById(messageId);
-            tmpMessage.innerHTML = "";
-            init.messageNumber += -1;
-            init.messages[messageId] = null;
             
+            //hämta meddelandet medhjälp av id
+            var tmpMessage = document.getElementById(messageId);
+            
+            //låt webbläsaren ta bort innehållet
+            tmpMessage.innerHTML = "";
+            
+            //räkna ned antalet meddelanden
+            this.messageNumber += -1;
+            
+            //ta bort referensen till objektet i listan (index-numret har samma nummer som id:t)
+            this.messages[messageId] = null;
+            
+            //ta bort elementet div
             tmpMessage.parentNode.removeChild(tmpMessage);
+            
+            //uppdatera numret
             init.init();
         }
         else{
             return;
         }
     },
+    
+    //funktion för hämtandet av tid
     time: function(messageId){
+        
+        //hämta meddelandet
         var message = init.messages[messageId];
+        
+        //hämta tid
         var time = message.getDate();
+        
+        //skriv ut
         alert("Inlägget är skapat den " + time.getDate() + " " + (init.month[time.getMonth()]) +" "+ time.getFullYear() + " klockan " + message.getDateText());
         
     },
+    
+    //funktion som hanterar knapp-ned-tryckningar
     keypressed: function (key){
         
+        //om enter trycktes och INTE shift
         if (!key.shiftKey && key.keyCode === 13) {
+            
+            //kör samma procedur som skicka-knappen
             init.run();
+            
+            //undvik webläsarens defaulthändelser (byta rad i detta fall) 
+            key.preventDefault();
+            
         }
+        return false;
     },
 };
--- 2-labbymezzage/JS/script.js
+++ 2-labbymezzage/JS/script.js
@@ -2,4 +2,12 @@
 
 window.onload = function () {
     init.init();
+    
+    //hämta lite element
+    var input = document.getElementById('inputField');
+    var button = document.getElementById('run');
+    
+    //lägg till lyssnaren till elementen
+    button.addEventListener("click", init.run);
+    input.addEventListener("keypress",init.keypressed);
 };
