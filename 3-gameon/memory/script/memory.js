cb63e611c7b50613436ee3161f5c698882a3b89b
mi222eh <mi222eh@student.lnu.se>
2014-11-24 16:36:06 +0000

    Concept of a board layout

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -1,9 +1,55 @@
 "use strict"
 
 function MemoryGame(row, col, gameID, gameLayoutID){
-    imgPosition = RandomGenerator.getPictureArray(row, col);
     
-    var area = document.getElementById(gameID);
+    this.getRow = function(){return row};
+    this.getCol = function(){return col};
+    
+    var THIS = this;
+    
+    this.imgPosition = RandomGenerator.getPictureArray(row, col);
+    
+    this.area = document.getElementById(gameID);
+    
+    MemoryGame.prototype.generateGame = function(){
+        
+        if (gameLayoutID === 0) {
+            THIS.area.appendChild(THIS.layout0());
+        }
+        else if (gameLayoutID === 1) {
+            THIS.area.appendChild(THIS.layout1());
+        }
+    };
+    MemoryGame.prototype.layout0 = function(){
+        
+    };
+    MemoryGame.prototype.layout1 = function(){
+        var picture = 0;
+        
+        var masterdiv = document.createElement('div');
+        for (var i = 0; i < THIS.getRow(); i+=1) {
+            
+            var rowdiv = document.createElement('div');
+            rowdiv.classList.add('row');
+            
+            for (var m = 0; m < THIS.getCol(); m+=1) {
+                var coldiv = document.createElement('div');
+                coldiv.classList.add(picture);
+                
+                var img = document.createElement('img');
+                img.setAttribute("src", "pics/" + THIS.imgPosition[picture] + ".png");
+                img.setAttribute("alt", "bricka nummer " + picture+1);
+                
+                coldiv.appendChild(img);
+                
+                rowdiv.appendChild(coldiv);
+                picture += 1;
+            }
+            masterdiv.appendChild(rowdiv);
+        }
+        return masterdiv;
+    };
+    
     
     
 }
--- 3-gameon/memory/script/script.js
+++ 3-gameon/memory/script/script.js
@@ -1,5 +1,7 @@
 "use strict"
 
 window.onload = function(){
+    var game1 = new MemoryGame(4, 4, "game1", 1);
     
+    game1.generateGame();
 };
