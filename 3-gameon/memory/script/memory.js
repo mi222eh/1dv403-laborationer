0f6b8891b09c76b2e9775f35ecd175e651eb3aca
mi222eh <mi222eh@student.lnu.se>
2014-11-26 18:55:38 +0000

    added comments

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -2,19 +2,36 @@
 
 function MemoryGame(row, col, gameID, gameLayoutID){
     
-    
-    var focusX = 0;
-    var focusY = 0;
+    //Variabler som kommer att användas
+    var focusX = 0;                                             //Fixerar fokuset (X)
+    var focusY = 0;                                             //Fixerar fokuset (Y)
     var THIS = this;
     var memory_Values = [];                                     //ska lagra 2 värden (brickorna)
-    var numberOfSuccess = 0;
+    var numberOfSuccess = 0;                                    //lager för antalet succéer
     var numberOfTries = 0;
     
-    
+    //<-----           Metoder till variablerna            ------>//
     this.addnumberOfTries = function(i){numberOfTries += i;}
     this.getnumberOfTries = function(){return numberOfTries}
+    
     this.addnmberOfSuccess = function(i){numberOfSuccess += i};
     this.getnumberOfSuccess = function(){return numberOfSuccess};
+    
+    this.getgameLayoutID = function(){return gameLayoutID};
+    this.getgameID = function(){return gameID};
+    
+    this.getRow = function(){return row};
+    this.getCol = function(){return col};
+    
+    this.getPosition = function(i){return Position[i]};
+    this.setPosition = function(ref){Position.push(ref)};
+    
+    this.setmemoryValue = function(ref){memory_Values.push(ref)};
+    this.getmemoryValue = function(i){return memory_Values[i]};
+    this.getWholememoryValue = function(){return memory_Values};
+    this.clearmemoryValue = function(){memory_Values = []};
+    
+    //<-------          Metoder för fokuset             ----->//
     this.handleDown = function(){
         if ((focusY + 1) < (row)) {
             focusY += 1;
@@ -46,29 +63,18 @@ function MemoryGame(row, col, gameID, gameLayoutID){
         columnNumber.childNodes[0].focus();
     };
     
-    this.getgameLayoutID = function(){return gameLayoutID};
-    this.getgameID = function(){return gameID};
     
-    this.getRow = function(){return row};
-    this.getCol = function(){return col};
-    
-    this.getPosition = function(i){return Position[i]};
-    this.setPosition = function(ref){Position.push(ref)};
-    
-    this.setmemoryValue = function(ref){memory_Values.push(ref)};
-    this.getmemoryValue = function(i){return memory_Values[i]};
-    this.getWholememoryValue = function(){return memory_Values};
-    this.clearmemoryValue = function(){memory_Values = []};
-    
+    //                  Array för slumpade siffror
     var Position = RandomGenerator.getPictureArray(this.getRow(), this.getCol());
     
-    console.log(this.getPosition());
-    
+    //              Område för spelplanen
     this.area = document.getElementById(gameID);
 }
+
+    //<-------          Metod för att generera spelplanen   ------>//
     MemoryGame.prototype.generateGame = function(){
-        var layout;
         
+        //Generar allt gemensamt här...
         var gameInfoDiv = document.createElement('div');
         var textNode = document.createTextNode('Antal försök: ');
         var divtext = document.createElement('div');
@@ -82,54 +88,60 @@ function MemoryGame(row, col, gameID, gameLayoutID){
         gameInfoDiv.appendChild(divWin);
         this.area.appendChild(gameInfoDiv);
         
-        
+        //Beorende på ID, skapa layout
         if (this.getgameLayoutID() === 0) {
+            
             this.area.appendChild(this.layout0());
         }
         else if (this.getgameLayoutID() === 1) {
-            layout = this.layout1();
-            this.area.appendChild(layout);
+            
+            this.area.appendChild(this.layout1());
         }
     };
+    
+    // <------- För ID 0-------->//
     MemoryGame.prototype.layout0 = function(){
         
     };
+    
+    // <------- För ID 1-------->//
     MemoryGame.prototype.layout1 = function(){
         var _this = this;
+        
+        //Variabel för att hålla koll på antalet bilder...
         var picture = 0;
         
-        
-        
-        
-        
+        //En div som håller alla brickor
         var masterdiv = document.createElement('div');
         masterdiv.classList.add('bricks');
+        
+        //<------- Generar rad och kolumn beroende av parametrarna ------>//
         for (var i = 0; i < this.getRow(); i+=1) {
             
             var rowdiv = document.createElement('div');
-            rowdiv.classList.add('row');
+            rowdiv.classList.add('row');                    //klass skapas för stilmallen
             
             for (var m = 0; m < this.getCol(); m+=1) {
+                
+                //taggar
                 var coldiv = document.createElement('div');
-                
-                
                 var img = document.createElement('img');
+                var a = document.createElement('a');
                 
-                
+                //konfigurationer
                 img.setAttribute("alt", "?");
                 img.setAttribute("height", "24px");
                 img.setAttribute("width", "24px");
                 img.setAttribute("src", "pics/0.png");
                 img.classList.add("_" + picture);
                 
-                
-                var a = document.createElement('a');
                 a.setAttribute("href", "#");
                 this.eventListeners(a, this);
                         a.addEventListener("keypress", function(event) {
                         _this.navigation(event);
                  });
                 
+                //sätta dit
                 a.appendChild(img);
                 coldiv.appendChild(a);
                 
@@ -139,16 +151,12 @@ function MemoryGame(row, col, gameID, gameLayoutID){
             masterdiv.appendChild(rowdiv);
         }
         
-        
-        
-        
-        return masterdiv;
+        return masterdiv;           //returnera mäster-diven
     };
     
+    //<------- Metod för navigation ------->//
     MemoryGame.prototype.navigation = function(event){
         var code = event.keyCode;
-    
-        console.log(code);
         
         switch (code) {
             case 38:
@@ -175,26 +183,26 @@ function MemoryGame(row, col, gameID, gameLayoutID){
         }
     }
         
-        
+    //<--------------   Metod för klickhändelser (då en bricka ska vändas) -------->//
     MemoryGame.prototype.clickAction = function(event){
                 var _this = this;
-                console.log(event.currentTarget);
                 var a = event.currentTarget;
                 var image = a.childNodes[0];
                 var index = image.getAttribute('class').slice(1);
-                
-                console.log(index);
-                console.log(this.getPosition(index));
-                
-                image.setAttribute("src", "pics/" + this.getPosition(index) + ".png");
-                image.setAttribute("alt", this.getPosition(index));
-                
-                
                 var aClone = a.cloneNode(true);
+                
+                //Nya attribut på bilden sätts
+                image.setAttribute("alt", this.getPosition(index));
+                image.setAttribute("src", "pics/" + this.getPosition(index) + ".png");
+                
+                //Ersätt a med klonad a (för att ta bort listeners)
                 a.parentNode.replaceChild(aClone, a);
+                
+                //Hoppsan, "keypress" gick också bort
                 aClone.addEventListener("keypress", function(event) {
                     _this.navigation(event);
                 });
+                
                 this.checkValues(aClone.childNodes[0]);
                 
                 this.focus();
@@ -204,9 +212,15 @@ function MemoryGame(row, col, gameID, gameLayoutID){
         
     };
     
+    //<------------------- Kontroll av värden (och lagrande) ---------->//
     MemoryGame.prototype.checkValues = function(targetValue){
+        
         var _this = this;
+        
+        //Sätt in referensen till image-taggen i arrayen
         this.setmemoryValue(targetValue);
+        
+        //Kolla arrayens storlek (förutsätter att den blir högst två, men man vet aldrig)
         if (_this.getWholememoryValue().length >= 2) {
             
             if(_this.validateValues()){
@@ -217,6 +231,7 @@ function MemoryGame(row, col, gameID, gameLayoutID){
             else{
                 
                 setTimeout(this.flipBack, 1000, _this);
+                _this.clearmemoryValue();
                 return;
             
             }
@@ -224,14 +239,13 @@ function MemoryGame(row, col, gameID, gameLayoutID){
              
         }
     };
-    
+    //<----------------  Jämför värden i arrayen ------------->//
     MemoryGame.prototype.validateValues = function(){
         
+        //hämta ut till en lokal variabel
         var img1 = this.getmemoryValue(0);
         var img2 = this.getmemoryValue(1);
         
-            
-        console.log(this.getmemoryValue(0));   
         var pos1 = img1.getAttribute('class').slice(1);
         var pos2 = img2.getAttribute('class').slice(1);
         
@@ -244,17 +258,23 @@ function MemoryGame(row, col, gameID, gameLayoutID){
         }
     };
     
+    //<------------------ Ansvara för att vända tillbaka brickorna ------------>//
     MemoryGame.prototype.flipBack = function(_this){
-        console.log(_this.getWholememoryValue());
-        _this.getmemoryValue(0).setAttribute("src", "pics/0.png");
-        _this.getmemoryValue(1).setAttribute("src", "pics/0.png");
         
+        //sätt attribut och alt...
+        _this.getmemoryValue(0).setAttribute("src", "pics/0.png");
+        _this.getmemoryValue(0).setAttribute("alt", "?");
+        
+        _this.getmemoryValue(1).setAttribute("src", "pics/0.png");
+        _this.getmemoryValue(1).setAttribute("alt", "?");
+        
+        //Lägg till listeners igen
         _this.eventListeners(_this.getmemoryValue(0).parentNode, _this);
         _this.eventListeners(_this.getmemoryValue(1).parentNode, _this);
         
-        _this.clearmemoryValue();
     };
     
+    //<-------------- Lägger till en listener på taggen som kommer igenom (till clickAction) ------->//
     MemoryGame.prototype.eventListeners = function(el, _this){
         el.addEventListener("click", function(event){
             _this.clickAction(event);
@@ -263,4 +283,7 @@ function MemoryGame(row, col, gameID, gameLayoutID){
 
     };
     
+    MemoryGame.prototype.updateNumbers = function(){
+        
+    };
     
