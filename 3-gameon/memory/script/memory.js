1fadb18dbf27f97ffa08efd258f2c1866d121d56
mi222eh <mi222eh@student.lnu.se>
2014-11-26 18:26:40 +0000

    fixed flip bug (could flip flipped bricks)

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -7,7 +7,14 @@ function MemoryGame(row, col, gameID, gameLayoutID){
     var focusY = 0;
     var THIS = this;
     var memory_Values = [];                                     //ska lagra 2 värden (brickorna)
+    var numberOfSuccess = 0;
+    var numberOfTries = 0;
     
+    
+    this.addnumberOfTries = function(i){numberOfTries += i;}
+    this.getnumberOfTries = function(){return numberOfTries}
+    this.addnmberOfSuccess = function(i){numberOfSuccess += i};
+    this.getnumberOfSuccess = function(){return numberOfSuccess};
     this.handleDown = function(){
         if ((focusY + 1) < (row)) {
             focusY += 1;
@@ -61,6 +68,21 @@ function MemoryGame(row, col, gameID, gameLayoutID){
 }
     MemoryGame.prototype.generateGame = function(){
         var layout;
+        
+        var gameInfoDiv = document.createElement('div');
+        var textNode = document.createTextNode('Antal försök: ');
+        var divtext = document.createElement('div');
+        var divWin = document.createElement('div');
+        var divNumber = document.createElement('div');
+        
+        
+        divtext.appendChild(textNode);
+        divtext.appendChild(divNumber);
+        gameInfoDiv.appendChild(divtext);
+        gameInfoDiv.appendChild(divWin);
+        this.area.appendChild(gameInfoDiv);
+        
+        
         if (this.getgameLayoutID() === 0) {
             this.area.appendChild(this.layout0());
         }
@@ -76,6 +98,10 @@ function MemoryGame(row, col, gameID, gameLayoutID){
         var _this = this;
         var picture = 0;
         
+        
+        
+        
+        
         var masterdiv = document.createElement('div');
         masterdiv.classList.add('bricks');
         for (var i = 0; i < this.getRow(); i+=1) {
@@ -88,20 +114,21 @@ function MemoryGame(row, col, gameID, gameLayoutID){
                 
                 
                 var img = document.createElement('img');
+                
+                
                 img.setAttribute("alt", "?");
                 img.setAttribute("height", "24px");
                 img.setAttribute("width", "24px");
                 img.setAttribute("src", "pics/0.png");
                 img.classList.add("_" + picture);
+                
+                
                 var a = document.createElement('a');
                 a.setAttribute("href", "#");
-                a.addEventListener("click", function(event){
-                    _this.clickAction(event);
-                
-                });
-                a.addEventListener("keypress", function(event) {
-                    _this.navigation(event);
-                });
+                this.eventListeners(a, this);
+                        a.addEventListener("keypress", function(event) {
+                        _this.navigation(event);
+                 });
                 
                 a.appendChild(img);
                 coldiv.appendChild(a);
@@ -111,6 +138,10 @@ function MemoryGame(row, col, gameID, gameLayoutID){
             }
             masterdiv.appendChild(rowdiv);
         }
+        
+        
+        
+        
         return masterdiv;
     };
     
@@ -146,15 +177,28 @@ function MemoryGame(row, col, gameID, gameLayoutID){
         
         
     MemoryGame.prototype.clickAction = function(event){
-        
+                var _this = this;
                 console.log(event.currentTarget);
-                var image = event.currentTarget.childNodes[0];
+                var a = event.currentTarget;
+                var image = a.childNodes[0];
                 var index = image.getAttribute('class').slice(1);
+                
                 console.log(index);
                 console.log(this.getPosition(index));
+                
                 image.setAttribute("src", "pics/" + this.getPosition(index) + ".png");
                 image.setAttribute("alt", this.getPosition(index));
-                this.checkValues(event.currentTarget.childNodes[0]);
+                
+                
+                var aClone = a.cloneNode(true);
+                a.parentNode.replaceChild(aClone, a);
+                aClone.addEventListener("keypress", function(event) {
+                    _this.navigation(event);
+                });
+                this.checkValues(aClone.childNodes[0]);
+                
+                this.focus();
+                
      
        
         
@@ -167,13 +211,16 @@ function MemoryGame(row, col, gameID, gameLayoutID){
             
             if(_this.validateValues()){
                 
+                _this.clearmemoryValue();
             }
             
             else{
                 
                 setTimeout(this.flipBack, 1000, _this);
+                return;
             
             }
+            
              
         }
     };
@@ -202,42 +249,18 @@ function MemoryGame(row, col, gameID, gameLayoutID){
         _this.getmemoryValue(0).setAttribute("src", "pics/0.png");
         _this.getmemoryValue(1).setAttribute("src", "pics/0.png");
         
+        _this.eventListeners(_this.getmemoryValue(0).parentNode, _this);
+        _this.eventListeners(_this.getmemoryValue(1).parentNode, _this);
+        
         _this.clearmemoryValue();
     };
     
-    MemoryGame.prototype.focusListenerAdder = function(){
-        
-        document.getElementById(this.getgameID()).addEventListener("keypress", this.handleArrows);
+    MemoryGame.prototype.eventListeners = function(el, _this){
+        el.addEventListener("click", function(event){
+            _this.clickAction(event);
+            }
+        );
+
     };
     
-    MemoryGame.prototype.handleArrows = function(e){
-    var code = e.keyCode;
-    
-    console.log(code);
-    
-    switch (code) {
-        case 38:
-            this.handleUp();
-            e.preventDefault();
-            break;
-            
-        case 37:
-            this.handleLeft();
-            e.preventDefault();
-            break;
-        
-        case 39:
-            this.handleRight();
-            e.preventDefault();
-            break;
-        
-        case 40:
-            this.handleDown();
-            e.preventDefault();
-            break;
-        
-        default:
-            // code
-    } 
-    };
     
