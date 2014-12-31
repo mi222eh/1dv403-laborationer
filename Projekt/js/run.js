207ba8ef8c096bcc90c8993e9ef0fab980e9df9a
Michael Ingvarsson <mi222eh@student.lnu,se>
2014-12-31 16:05:07 +0100

    Fixed and added style, Finished and added functions
    * styled the window
    * added functions for loading and getting pictures
    * added exit button and picture

--- Projekt/js/run.js
+++ Projekt/js/run.js
@@ -14,6 +14,7 @@ var init = {
 		//link editing
 		link.addEventListener("click", function(e){
 			init.openWindow(e);
+			init.loadPictures();
 			return false;
 		});
 		link.setAttribute("href", "#");
@@ -27,6 +28,75 @@ var init = {
 		//create window
 
 		var window_  = document.createElement('div');
+		var statusbar = document.createElement('div');
 		var exitLink = document.createElement('a');
+		var exitimage = document.createElement('img');
+		var windowicon = document.createElement('img');
+		var name = document.createElement('p');
+		var text = document.createTextNode("Bildvisare");
+		var contentDiv = document.createElement('div');
+		var infoDiv = document.createElement('div')
+
+
+		window_.classList.add("window");
+		statusbar.classList.add("statusBar");
+		exitLink.classList.add("exitLink");
+		windowicon.classList.add("windowIcon");
+		name.classList.add("windowIconText");
+		contentDiv.classList.add("content");
+		infoDiv.classList.add("info");
+
+		windowicon.setAttribute("src", "pics/imageViewerIcon.png")
+
+		exitimage.setAttribute("src", "pics/exitPic.png");
+		exitimage.setAttribute("alt", "Stäng rutan");
+
+		exitLink.setAttribute("href", "#");
+		exitLink.addEventListener("click", function(e){
+			e.preventDefault();
+			window_.parentNode.removeChild(window_);
+		});
+
+
+		name.appendChild(text);
+
+		exitLink.appendChild(exitimage);
+
+		statusbar.appendChild(exitLink);
+		statusbar.appendChild(name);
+		statusbar.appendChild(windowicon);
+		
+
+		window_.appendChild(statusbar);
+		window_.appendChild(contentDiv);
+		window_.appendChild(infoDiv);
+
+		document.getElementById('windowmanager').appendChild(window_);
+
+		e.preventDefault();
+
+	},
+
+	loadPictures: function(){
+		var pictures = init.getPictures();
+	},
+
+	getPictures: function(){
+		var xhr = new XMLHttpRequest();
+
+		xhr.onreadystatechange = function(){
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    init.readServerMessage(xhr.responseText);
+                    
+                }
+                else{
+                    console.log("läsfel, status: " + xhr.status);
+                }
+            }
+            console.log("svar");
+        };
+        
+		xhr.open("GET", "http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/", true);
 	},
 }
